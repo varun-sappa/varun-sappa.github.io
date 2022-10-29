36dcfd642acbc4143915bfc97816214852acf887
varun-sappa <94960569+varun-sappa@users.noreply.github.com>
2022-10-29 16:33:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,128 @@
-//apprearance
-$("input.variation").on("click", function () {
-	if ($(this).val() > 3) {
-		$("body").css("background", "#111");
-		$("footer").attr("class", "dark");
-	} else {
-		$("body").css("background", "#f9f9f9");
-		$("footer").attr("class", "");
+document.getElementById('dropdownMenuButton2').addEventListener('click',()=>{
+	const element = document.getElementById("test");
+    // element.classList.remove("show");
+		if(element.classList.contains("show")){
+		element.classList.remove("show");
+	}else{
+		element.classList.add("show");
 	}
+
 });
 
-// toggle list vs card view
-$(".option__button").on("click", function () {
-	$(".option__button").removeClass("selected");
-	$(this).addClass("selected");
-	if ($(this).hasClass("option--grid")) {
-		$(".results-section").attr("class", "results-section results--grid");
-	} else if ($(this).hasClass("option--list")) {
-		$(".results-section").attr("class", "results-section results--list");
-	}
+
+const abc=document.getElementById('drop');
+abc.addEventListener('click',()=>{
+	const element = document.getElementById("test2");
+//   element.classList.remove("dropdown-item-test");
+if(element.classList.contains("dropdown-item-test")){
+	element.classList.remove("dropdown-item-test");
+}else{
+	element.classList.add("dropdown-item-test");
+}
 });
+
+
+
+
+
+
+document.getElementById('dropdownMenuButton3').addEventListener('click',()=>{
+	const element = document.getElementById("test3");
+    // element.classList.remove("show");
+		if(element.classList.contains("show")){
+		element.classList.remove("show");
+	}else{
+		element.classList.add("show");
+	}
+
+});
+
+
+const xyz=document.getElementById('drop');
+abc.addEventListener('click',()=>{
+	const element = document.getElementById("test2");
+//   element.classList.remove("dropdown-item-test");
+if(element.classList.contains("dropdown-item-test")){
+	element.classList.remove("dropdown-item-test");
+}else{
+	element.classList.add("dropdown-item-test");
+}
+});
+
+
+document.getElementById('dropdownMenuButton4').addEventListener('click',()=>{
+	const element = document.getElementById("test4");
+    // element.classList.remove("show");
+		if(element.classList.contains("show")){
+		element.classList.remove("show");
+	}else{
+		element.classList.add("show");
+	}
+
+});
+
+
+const lmn=document.getElementById('drop');
+abc.addEventListener('click',()=>{
+	const element = document.getElementById("test2");
+//   element.classList.remove("dropdown-item-test");
+if(element.classList.contains("dropdown-item-test")){
+	element.classList.remove("dropdown-item-test");
+}else{
+	element.classList.add("dropdown-item-test");
+}
+});
+
+
+
+
+document.getElementById('dropdownMenuButton5').addEventListener('click',()=>{
+	const element = document.getElementById("test5");
+    // element.classList.remove("show");
+		if(element.classList.contains("show")){
+		element.classList.remove("show");
+	}else{
+		element.classList.add("show");
+	}
+
+});
+
+
+const mno=document.getElementById('drop');
+abc.addEventListener('click',()=>{
+	const element = document.getElementById("test2");
+//   element.classList.remove("dropdown-item-test");
+if(element.classList.contains("dropdown-item-test")){
+	element.classList.remove("dropdown-item-test");
+}else{
+	element.classList.add("dropdown-item-test");
+}
+});
+
+
+// });
+//apprearance
+// $("input.variation").on("click", function () {
+// 	if ($(this).val() > 3) {
+// 		$("body").css("background", "#111");
+// 		$("footer").attr("class", "dark");
+// 	} else {
+// 		$("body").css("background", "#f9f9f9");
+// 		$("footer").attr("class", "");
+// 	}
+// });
+
+
+// toggle list vs card view
+// $(".option__button").on("click", function () {
+// 	$(".option__button").removeClass("selected");
+// 	$(this).addClass("selected");
+// 	if ($(this).hasClass("option--grid")) {
+// 		$(".results-section").attr("class", "results-section results--grid");
+// 	} else if ($(this).hasClass("option--list")) {
+// 		$(".results-section").attr("class", "results-section results--list");
+// 	}
+// });
+
 
 
